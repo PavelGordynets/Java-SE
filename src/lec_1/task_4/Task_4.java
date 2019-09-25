8a72aa50e245a23d1a3da08b0a688bb8f65c229a
PavelGordynets <P,gordynets@gmail.com>
2019-09-25 13:27:31 +0300

    initial commit

---
+++ src/lec_1/task_4/Task_4.java
@@ -0,0 +1,22 @@
+package lec_1.task_4;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 3;
+        int number3 = 3;
+        int counterPositiveNumber = 0;
+        if (number1 > 0 ){
+            counterPositiveNumber++;
+        }
+        if (number2 > 0 ){
+            counterPositiveNumber++;
+        }
+        if (number3 > 0 ){
+            counterPositiveNumber++;
+        }
+        System.out.println(counterPositiveNumber);
+    }
+
+
+}
