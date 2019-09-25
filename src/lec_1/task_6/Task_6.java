7affe540c316266eca891e48ab969c288975fbe9
PavelGordynets <P,gordynets@gmail.com>
2019-09-25 15:17:23 +0300

    Lec_1_Task_6

---
+++ src/lec_1/task_6/Task_6.java
@@ -0,0 +1,13 @@
+package lec_1.task_6;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        int numberA = 33;
+        int numberB = 5;
+        if (numberA > numberB) {
+            System.out.println("Число А=" + numberA + " больше числа B=" + numberB);
+        } else {
+            System.out.println("Число B=" + numberB + " больше числа A=" + numberA);
+        }
+    }
+}
