666ab65eb56d1693885547667abf0031307d8451
PavelGordynets <P,gordynets@gmail.com>
2019-09-27 13:19:40 +0300

    Lec_2_Task_6

---
+++ src/lec_2/task_6/Task_6.java
@@ -0,0 +1,11 @@
+package lec_2.task_6;
+//Напишите программу вывода всех четных чисел от 2 до 100
+//включительно
+
+public class Task_6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
