bdc84206ad428f78c36e1ae4bb4f7b463bb954bc
PavelGordynets <P,gordynets@gmail.com>
2019-09-27 13:22:53 +0300

    Lec_2_Task_6

---
+++ src/lec_2/task_7/Task_7.java
@@ -0,0 +1,11 @@
+package lec_2.task_7;
+//Напишите программу, определяющую сумму всех нечетных чисел от 1
+//до 99
+
+public class Task_7 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
